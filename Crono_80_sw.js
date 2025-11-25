d7d3c722800b2dc7949560964144fed7310bf3c5
rbenet71 <62558124+rbenet71@users.noreply.github.com>
2025-11-25 15:37:21 +0100

    PWA Pictos App

---
+++ Crono_80_sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'crono-80-v1';
+const urlsToCache = [
+  './Crono_80.html',
+  './Crono_80_192x192.png',
+  './Crono_80_manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
