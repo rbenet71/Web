1216a65f260a98f658b1b47f10d125f3df95360e
rbenet71@gmail.com <rbenet71@gmail.com>
2026-01-05 01:10:37 +0100

    Numero Version

--- Ciclismo/Dashcam/Dashcam_service-worker.js
+++ Ciclismo/Dashcam/Dashcam_service-worker.js
@@ -1,12 +1,12 @@
-// Service Worker para Dashcam iPhone Pro v4.14.1
-const CACHE_NAME = 'dashcam-iphone-pro-cache-v4.14.1';
+// Service Worker para Dashcam iPhone Pro v4.2
+const CACHE_NAME = 'dashcam-iphone-pro-cache-v4.2';
 const urlsToCache = [
     './',
-    './Dashcam.html?v=4.14.1',
-    './Dashcam_styles.css?v=4.14.1',
-    './Dashcam_App.js?v=4.14.1',
-    './Dashcam_manifest.json?v=4.14.1',
-    '/Dashcam_ayuda_completa.html?v=4.14.1',
+    './Dashcam.html?v=4.2',
+    './Dashcam_styles.css?v=4.2',
+    './Dashcam_App.js?v=4.2',
+    './Dashcam_manifest.json?v=4.2',
+    '/Dashcam_ayuda_completa.html?v=4.2',
     './recursos/Logo_Dashcam_Bike_192x192.png',
     './recursos/Picto_Color_192x192.png',
     './recursos/Picto_Color_512x512.png'
